2d05c432ee6fcea9818c024c9d1a9d29774bd938
Chen Zu <chen0zu@gmail.com>
2018-03-20 22:38:23 +1100

    tree

--- tree.py
+++ tree.py
@@ -23,3 +23,31 @@ def calcShannonEnt(dataSet):
 		shannonEnt -= prob * log(prob,2)
 
 	return shannonEnt
+
+def splitDataSet(dataSet, axis, value):
+	retDataSet = []
+	for featVec in dataSet:
+		if featVec[axis] == value:
+			reducedFeatVec = featVec[:axis]
+			reducedFeatVec.extend(featVec[axis+1:])
+			retDataSet.append(reducedFeatVec)
+	return retDataSet
+
+def chooseBestFeatureToSplit(dataSet):
+	numFeatures = len(dataSet[0]) - 1
+	baseEntropy = calcShannonEnt(dataSet)
+	bestInfoGain = 0.0
+	bestFeature = -1
+	for i in range(numFeatures):
+		featList = [example[i] for example in dataSet]
+		uniqueVals = set(featList)
+		newEntropy = 0.0
+		for value in uniqueVals:
+			subDataSet = splitDataSet(dataSet, i, value)
+			prob = len(subDataSet)/float(len(dataSet))
+			newEntropy += prob * calcShannonEnt(subDataSet)
+		infoGain = baseEntropy - newEntropy
+		if infoGain > bestInfoGain:
+			bestInfoGain = infoGain
+			bestFeature = i
+	return bestFeature
