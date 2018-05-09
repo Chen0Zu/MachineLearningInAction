1712c9eccbe66a0af7649ecdf45302dc6c1f0a19
Chen Zu <chen0zu@gmail.com>
2018-05-09 11:45:48 +1000

    update

---
+++ mrMeanMapper.py
@@ -0,0 +1,17 @@
+import sys
+from numpy import *
+import pdb
+
+def read_input(file):
+	for line in file:
+		yield line.rstrip()
+
+input = read_input(sys.stdin)
+# pdb.set_trace()
+input = [float(line) for line in input]
+numInputs = len(input)
+input = mat(input)
+sqInput = power(input,2)
+
+print "%d\t%f\t%f" % (numInputs, mean(input), mean(sqInput))
+print >> sys.stderr, "report: still alive"
